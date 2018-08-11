e8937f89759809c1658888e19bda7fe52e3102d0
krysX <siposkf@gmail.com>
2018-08-11 13:01:08 +0200

    Standard player controller
    Using standard  player controller beacuse the space one would be too clunky

--- LD42/Assets/Scripts/Player.cs
+++ LD42/Assets/Scripts/Player.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     Rigidbody rb;
-    GameObject[] walls;
     bool grounded;
 
-    public float jumpSpeed;
+    public float jumpVelocity;
     public float speed;
 
     public float mouseXspeed;
     public float mouseYspeed;
-    public float turnSpeed;
 
     
 
@@ -21,48 +19,25 @@ public class Player : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody>();
         Camera.main.transform.localPosition = Vector3.zero;
-        walls = GameObject.FindGameObjectsWithTag("Wall");
     }
 
     private void Update()
     {
         //Mouse X
         float mouseInputX = Input.GetAxisRaw("Mouse X");
-        Camera.main.transform.eulerAngles += Vector3.up * mouseInputX * mouseXspeed * Time.deltaTime;
+        transform.eulerAngles += Vector3.up * mouseInputX * mouseXspeed * Time.deltaTime;
         //Mouse Y
         float mouseInputY = Input.GetAxisRaw("Mouse Y");
         Camera.main.transform.eulerAngles += Vector3.left * mouseInputY * mouseYspeed * Time.deltaTime;
         //Jump
         if(Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            rb.AddForce(jumpSpeed * transform.up);
+            rb.AddForce(jumpVelocity * transform.up);
         }
 
-        if(!grounded)
-        { 
-            if(rb.velocity != Vector3.zero)
-            {
-                rb.velocity = speed * transform.forward;
-            }
-
-            if(Input.GetMouseButton(0))
-            {
-                rb.velocity = Vector3.zero;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, 0.5f))
-                {
-                    if (hit.collider.gameObject.tag == "Wall")
-                    {
-                        transform.position = Vector3.MoveTowards(transform.position, hit.point, speed * Time.deltaTime);
-                    }
-                }
-            }
-        }
-
-        //Player rotation
-        Vector3 inputRotation = new Vector3(-Input.GetAxisRaw("Vertical"), Input.GetAxis("Horizontal"), 0).normalized;
-        transform.eulerAngles += inputRotation * speed * Time.deltaTime;
+        //Player Movement
+        Vector3 inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        transform.Translate(inputDir * speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision c)
